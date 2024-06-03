dbd25a3147405870517ce2b6331dffe26571fd34
lyxc <103878931+yiyanglaw@users.noreply.github.com>
2024-06-03 08:16:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 from flask import Flask, request
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 import re
 import string
+import numpy as np
 
 app = Flask(__name__)
 
-# Load the spam detection model
+# Load the data for training
+data = pd.read_csv('sms_3.csv')
+
+# Fill missing values with an empty string
+data['Message'] = data['Message'].fillna('')
+
+data['Spam'] = data['Category'].apply(lambda x: 1 if x == 'spam' else 0)
+
+X_train, X_test, y_train, y_test = train_test_split(data.Message, data.Spam, test_size=0.25)
+
+# Create a pipeline with CountVectorizer and MultinomialNB
 clf = Pipeline([
     ('vectorizer', CountVectorizer()),
     ('nb', MultinomialNB())
 ])
 
-# Sample data to test the model
-data = pd.DataFrame({
-    'Message': ['how are u?', 'Congratulations! You have won a free trip. Claim it now!'],
-    'Spam': [0, 1]
-})
-
 # Training the model
-clf.fit(data.Message, data.Spam)
+clf.fit(X_train, y_train)
 
 def preprocess_text(text):
     text = text.translate(str.maketrans('', '', string.punctuation)).lower()
