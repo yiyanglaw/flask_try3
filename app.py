9c31f29eaff0d4aff89e7b998262c3725014b57c
lyxc <103878931+yiyanglaw@users.noreply.github.com>
2024-06-03 12:34:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
